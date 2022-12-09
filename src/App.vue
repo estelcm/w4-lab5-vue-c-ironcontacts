fd5661a6a89098dce25454be24ee08f9f6901433
Estel CM <estel_927@hotmail.com>
2022-12-09 17:45:27 +0100

    iteration 2

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,68 @@
+// src/App.vue
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
 
-  <i class="fas fa-trophy"></i>
+<h1>IronContacts</h1>
+
+<button @click="randomContact">Add Random Contact</button>
+  <table>
+    <thead>
+      <tr>
+        <th>Picture </th>
+        <th>Name</th>
+        <th>Popularity </th>
+        <th>Won an Oscar</th>
+        <th>Won anEmmy</th>
+        <th>Actions</th>
+        
+
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(contact, index) in contactList" :key="index" >
+        <td> <img :src="contact.pictureUrl" :alt="contact.pictureUrl" width="70"/> </td>
+         <td>{{ contact.name }}</td> 
+          <td>{{ contact.popularity }}</td>
+          <td v-if="(contact.wonOscar===true )"><i class="fas fa-trophy"></i></td>
+          <td v-if="(contact.wonEmmy===true )"><i class="fas fa-trophy"></i></td>
+          <td>  <button @click="borrarElemento(index)" type="button">
+              <i class="fa-solid fa-trash"></i></button></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import contacts from "./contacts.json";
 export default {
+  data() {
+    return {
+      contacts: contacts,
+      contactList: contacts.slice(0,5),
+    };
+  },
+  computed:{
+    newRandomContact(){
+      const contactLength = this.contacts.length;
+    function randomIntFromInterval(6, contactLength ) { // min and max included 
+  return Math.floor(Math.random() * (contactLenght - 6 + 1) + 6)};
 
-}
+//    addRandomContact(){
+//     const contactLength =this.contacts.length;
+//     return Math.floor(Math.random() * contactLength);
+//    },
+// randomContact(){
+//   if(!this.contactList.includes(addRandomContact)){
+//     this.contactList.push(addRandomContact);
+//   }
+//   return randomContact;
+// }
+  }}
+//  methods:{
+//   contactList(contact){
+//     contactList.slice(0,5);
+//   }
+//  }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
